58704dd1c1448e83414928522c15e93a4c232cf8
Christian Fink <christian.fink@bluewin.ch>
2018-05-31 11:40:54 +0200

    Init

--- MovieList.py
+++ MovieList.py
@@ -5,7 +5,7 @@
 # @Date:   2018-05-18T09:40:20+02:00
 # @Email:  christian.fink@bluewin.ch
 # @Last modified by:   christian
-# @Last modified time: 2018-05-19T08:06:45+02:00
+# @Last modified time: 2018-05-19T12:43:31+02:00
 
 # import time
 # import sys
@@ -33,7 +33,8 @@ class DownloadThread(QtCore.QThread):
             'outtmpl': destName,
             'noplaylist': True,
             'quiet': True,
-            'progress_hooks': [self.myHook]
+            'progress_hooks': [self.myHook],
+            'prefer-ffmpeg': True
         }
         self.url = url
         self.subClips = 2
@@ -42,16 +43,9 @@ class DownloadThread(QtCore.QThread):
     def __del__(self):
         self.wait()
 
-    # def setData(self, url, destName):
-    #     self.__ydl_opts['outtmpl'] = destName
-    #     self.url = url
-    #     self.subClips = 2
-
     def run(self):
-        print("Thread wurde gestartet")
         with youtube_dl.YoutubeDL(self.__ydl_opts) as ydl:
             ydl.download([self.url])
-        print("Thread wurde beendet")
 
     def myHook(self, d):
         if d['status'] == 'finished':
@@ -77,17 +71,13 @@ class MovieListWidget(QtWidgets.QListWidget):
         self.MovieList = {}
         self.downloadActive = False
         self.__activeItem = None
-        # self.__thread = DownloadThread()
         self.__thread = None
-
 
     @QtCore.pyqtSlot()
     def startDownload(self):
         if self.downloadActive:
-            print("Download noch Aktiv")
             return False
         if self.count() == 0:
-            print("Keine weitern Elemente vorhanden")
             return False
 
         self.downloadActive = True
@@ -111,220 +101,40 @@ class MovieListWidget(QtWidgets.QListWidget):
 
     @QtCore.pyqtSlot()
     def download_end(self):
-        print("Download beendet")
         self.takeItem(0)
         self.__activeItem = None
         self.downloadActive = False
         self.__thread.download_end.disconnect(self.download_end)
-        self.__thread.download_progress.discconnect(self.download_progress)
-        print(50*"=")
-        print("Download neu starten")
+        self.__thread.download_progress.disconnect(self.download_progress)
         self.startDownload()
 
-    def addURL(self, url):
-        if url in self.MovieList:
+    def addURL(self, data):
+        # for data in self.__mainContent(url):
+        if data['url'] in self.MovieList:
             return False
-        content = self.__getContent(url)
-        if not content:
-            return False
-
-        if content['urlType'] == "film":
-            self.MovieList[url] = {
-                'url': url,
-                'progress_video': 0.0,
-                'progress_audio': 0.0,
-                'destName': "",
-                'title': "",
-                'content': content,
-                'image': None,
-                'pixmap': None,
-                'hls': content['hls'].replace("\\", ""),
-                'dash': content['dash'].replace("\\", "")
-            }
-            self.MovieList[url]['image'] = content.get('image')
-            self.MovieList[url]['destName'] = "{0}.%(ext)s".format(
-                content.get('title')
-            )
-            self.MovieList[url]['title'] = content.get('title')
-            if self.MovieList[url]['image']:
-                r = requests.get(self.MovieList[url]['image'])
-                if r.status_code == 200:
-                    pixmap = QtGui.QPixmap()
-                    pixmap.loadFromData(r.content, "JPG")
-                    self.MovieList[url]['pixmap'] = pixmap
-            self.__add_Element(self.MovieList[url])
-        elif content['urlType'] == "serie":
-            return False
-        elif content['urlType'] == "season":
-            return False
-        elif content['urlType'] == "episode":
-            self.MovieList[url] = {
-                'url': url,
-                'progress_video': 0.0,
-                'progress_audio': 0.0,
-                'destName': "",
-                'title': "",
-                'content': content,
-                'image': None,
-                'pixmap': None,
-                'hls': content['hls'].replace("\\", ""),
-                'dash': content['dash'].replace("\\", "")
-            }
-            self.MovieList[url]['image'] = content.get('image')
-            title = "{0} - S{1:02d} E{2:02d} - {3}".format(
-                content['serie']['title'],
-                content['season'],
-                content['episode'],
-                content['episodeTitle']
-            )
-            self.MovieList[url]['destName'] = "{0}.%(ext)s".format(
-                title
-            )
-            self.MovieList[url]['title'] = title
-            if content['serie']['image']:
-                r = requests.get(content['serie']['image'])
-                if r.status_code == 200:
-                    pixmap = QtGui.QPixmap()
-                    pixmap.loadFromData(r.content, "JPG")
-                    self.MovieList[url]['pixmap'] = pixmap
-            self.__add_Element(self.MovieList[url])
-
+        pixmap = QtGui.QPixmap()
+        r = requests.get(data['image'])
+        if r.status_code == 200:
+            pixmap.loadFromData(r.content, "JPG")
+        self.MovieList[data['url']] = {
+            'url': data['url'],
+            'progress_video': 0.0,
+            'progress_audio': 0.0,
+            'destName': "{0}.%(ext)s".format(data['title']),
+            'title': data['title'],
+            'content': data['contentType'],
+            'image': data['image'],
+            'pixmap': pixmap,
+            'hls': data['hls'],
+            'dash': data['dash']
+        }
+        self.__add_Element(self.MovieList[data['url']])
         return True
-
-    def __getContent(self, url):
-        r = requests.get(url)
-        details = {}
-        if r.status_code != 200:
-            return False
-        soup = BeautifulSoup(r.text, 'html.parser')
-        breadCrumb = url.split("/")[3:]
-        details['contentType'] = breadCrumb[0]
-        if details['contentType'] == "serien":
-            """
-                Listenlänge = 4 : Episode
-                Listenlänge = 3 : Staffel
-                Listenlänge = 2 : Serie
-                Listenlänge = 1 : Ungültig
-            """
-            if len(breadCrumb) == 1:
-                return False
-            _l = ['http://watchbox.de', 'serien', breadCrumb[1]]
-            if len(breadCrumb) == 2:
-                """ Abfrage für Staffel und Episode erstellen """
-                return False
-                url_serie = url
-                url_staffel = None
-                url_episode = None
-
-                details['urlType'] = "serie"
-            elif len(breadCrumb) == 3:
-                """ Abfrage für Episode erstellen """
-                return False
-                url_serie = "/".join(_l)
-                url_staffel = url
-                url_episode = None
-                details['urlType'] = "season"
-            elif len(breadCrumb) == 4:
-                url_serie = "/".join(_l)
-                _l.append(breadCrumb[2])
-                url_staffel = "/".join(_l)
-                url_episode = url
-                m = re.search("-(\d*)\.html", url_episode)
-                if m:
-                    details['id_episode'] = int(m.group(1))
-                    _ep = soup.find(
-                        "a",
-                        class_="teaserset__teaser",
-                        attrs={'data-movie-id': str(details['id_episode'])}
-                    )
-                    if _ep:
-                        _info = _ep.find("div", class_='teaser__season-info')
-                        if _info:
-                            m = re.search(
-                                "Staffel (\d*), Episode (\d*)",
-                                _info.text.replace("\n", "").strip()
-                            )
-                            if m:
-                                details['season'] = int(m.group(1))
-                                details['episode'] = int(m.group(2))
-                        _header = _ep.find("div", class_="teaser__header")
-                        if _header:
-                            details['episodeTitle'] = _header.text.replace(
-                                "\n", ""
-                            ).strip()
-                    details['urlType'] = "episode"
-            details['serie'] = self.__getSerienContent(url_serie)
-
-            # print("Serie    : {0}".format(url_serie))
-            # print("Staffel  : {0}".format(url_staffel))
-            # print("Episode  : {0}".format(url_episode))
-        elif details['contentType'] == "filme":
-            _image = soup.find("div", attrs={'class': "video-details__image"})
-            if _image:
-                _img = _image.find('img')
-                if _img['src'].startswith("/"):
-                    _img['src'] = "http:{0}".format(_img['src'])
-                details['image'] = _img["src"]
-                details['title'] = _img['alt']
-                details['urlType'] = "film"
-            #
-            # EPISODEN BILD
-            #     _image = soup.find("meta", attrs={'name': "twitter:image"})
-            #     if _image:
-            #         details['image'] = _image['content']
-            #     _title = soup.find("meta", attrs={'name': "twitter:title"})
-            #     if _title:
-            #         details['title'] = _title['content'].replace(
-            #             " im Online-Stream", ""
-            #         )
-
-        for _js in soup.find_all('script', attrs={'type': 'text/javascript'}):
-            if _js.get("src", None):
-                continue
-            m = re.search("var playerConf = ({.*);", _js.text)
-            if not m:
-                continue
-            playerConf = json.loads(m.group(1))
-            details['hls'] = playerConf['source']['hls']
-            details['dash'] = playerConf['source']['dash']
-            break
-        return details
-
-    def __getSerienContent(self, url):
-        r = requests.get(url)
-        if r.status_code != 200:
-            return None
-        soup = BeautifulSoup(r.text, 'html.parser')
-        details = {'seasons': []}
-        _image = soup.find("div", attrs={'class': "video-details__image"})
-        if _image:
-            _img = _image.find('img')
-            if _img['src'].startswith("/"):
-                _img['src'] = "http:{0}".format(_img['src'])
-            details['image'] = _img["src"]
-            details['title'] = _img['alt']
-
-        _seasons = soup.find('ul', class_="season-panel")
-        if _seasons:
-            details['seasons'] = [
-                {
-                    'nr': int(season['data-season-number']),
-                    'title': season.text.replace("\n", "").strip()
-                }
-                for season in _seasons.find_all(
-                    "li",
-                    class_="season-panel__item"
-                )
-            ]
-        return details
 
     def __add_Element(self, data):
         itemN = MovieListWidgetItem(data, self)
         self.addItem(itemN)
         self.setItemWidget(itemN, itemN.widget)
-
-    def __add_Episode(self, data):
-        pass
 
 class MovieListWidgetItem(QtWidgets.QListWidgetItem):
 
--- WatchBoxDownload.py
+++ WatchBoxDownload.py
@@ -5,7 +5,7 @@
 # @Date:   2018-05-18T08:45:21+02:00
 # @Email:  christian.fink@bluewin.ch
 # @Last modified by:   christian
-# @Last modified time: 2018-05-18T17:15:18+02:00
+# @Last modified time: 2018-05-19T12:45:41+02:00
 
 # import time
 # import sys
@@ -18,8 +18,10 @@ from PyQt5 import QtWidgets, QtGui, QtCore
 import gi
 from gi.repository import GObject
 from formular_ui import Ui_Form
-
-
+import requests
+from bs4 import BeautifulSoup
+import json
+import re
 
 class MainWidget(QtWidgets.QWidget):
 
@@ -31,20 +33,251 @@ class MainWidget(QtWidgets.QWidget):
         self.ui.btn_Insert.clicked.connect(self._insert)
         self.ui.btn_Download.clicked.connect(self._startDownload)
         self.show()
-        self.ui.entry_URL.setText("https://www.watchbox.de/serien/yosuga-no-sora-15375/staffel-1/zurueck-in-die-vergangenheit-373928.html")
+        # self.ui.entry_URL.setText("https://www.watchbox.de/serien/yosuga-no-sora-15375/staffel-1/zurueck-in-die-vergangenheit-373928.html")
 
     @QtCore.pyqtSlot()
     def _insert(self):
         url = self.ui.entry_URL.text()
-        success = self.ui.list_URL.addURL(url)
-        if not success:
-            print("URL \"{0}\" existert bereits".format(url))
+        for data in self.__mainContent(url):
+            success = self.ui.list_URL.addURL(data)
+            print("{0} : {1}".format(data['title'], success))
         self.ui.entry_URL.setText("")
 
     @QtCore.pyqtSlot()
     def _startDownload(self):
-        a = self.ui.list_URL.startDownload()
-        print(a)
+        self.ui.list_URL.startDownload()
+
+    def __mainContent(self, url):
+        if url.endswith("/"):
+            url = url[0:-1]
+        breadCrumb = url.split("/")[3:]
+        details = {
+            'contentType': breadCrumb[0],
+            'url': url
+        }
+        r = requests.get(url)
+        if r.status_code != 200:
+            return False
+        soup = BeautifulSoup(r.text, 'html.parser')
+
+        _image = soup.find("div", attrs={'class': "video-details__image"})
+        if _image:
+            _img = _image.find('img')
+            if _img['src'].startswith("/"):
+                _img['src'] = "http:{0}".format(_img['src'])
+                details['image'] = _img["src"]
+                details['title'] = _img['alt']
+
+                if details['contentType'] == "filme":
+                    details['destName'] = "{0}.%(ext)s".format(
+                        details['title']
+                    )
+        _dataAsset = soup.find("div", class_="big-teaser-video")
+        if _dataAsset:
+            details['data-asset-id'] = _dataAsset['data-asset-id']
+
+        for _js in soup.find_all('script', attrs={'type': 'text/javascript'}):
+            if _js.get("src", None):
+                continue
+            m = re.search("var playerConf = ({.*);", _js.text)
+            if not m:
+                continue
+            playerConf = json.loads(m.group(1))
+            details['hls'] = playerConf['source']['hls']
+            details['dash'] = playerConf['source']['dash']
+            break
+
+        if details['contentType'] == "filme":
+            yield details
+        elif details['contentType'] == "serien":
+            _SerienURL = ['http://watchbox.de', 'serien', breadCrumb[1]]
+            if 'dash' in details:
+                """
+                url war Episode
+                Serientitel, Bild und Episodentitel ermitteln
+                """
+                serien_content = self.__serienContent("/".join(_SerienURL))
+                episoden_content = self.__episodeContent(url, soup)
+                details['title'] = "{0} - S{1:02d} E{2:02d} - {3}".format(
+                    serien_content['title'],
+                    episoden_content['season'],
+                    episoden_content['episode'],
+                    episoden_content['episodeTitle']
+                )
+                details['image'] = serien_content['image']
+                yield details
+            else:
+                """
+                url war Staffel oder Serie
+                Staffelinhalt oder Serieninhalt ermitteln
+                und alle Episoden zurückgeben
+                """
+                serien_content = self.__serienContent(url)
+                if len(breadCrumb) == 2:
+                    for season in serien_content['seasons']:
+                        for episode in self.__staffelContent(
+                            season['url'],
+                            details['data-asset-id']
+                        ):
+                            episoden_content = self.__episodeContent(episode['url'])
+                            yield {
+                                'url': episode['url'],
+                                'contentType': 'serien',
+                                'hls': episoden_content['hls'],
+                                'dash': episoden_content['dash'],
+                                'title': "{0} - S{1:02d} E{2:02d} - {3}".format(
+                                    serien_content['title'],
+                                    episoden_content['season'],
+                                    episoden_content['episode'],
+                                    episoden_content['episodeTitle']
+                                ),
+                                'image': serien_content['image']
+                            }
+                elif len(breadCrumb) == 3:
+                    staffelContent = self.__staffelContent(
+                        url,
+                        details['data-asset-id'],
+                        soup
+                    )
+                    for episode in staffelContent:
+                        episoden_content = self.__episodeContent(episode['url'])
+                        yield {
+                            'url': episode['url'],
+                            'contentType': 'serien',
+                            'hls': episoden_content['hls'],
+                            'dash': episoden_content['dash'],
+                            'title': "{0} - S{1:02d} E{2:02d} - {3}".format(
+                                serien_content['title'],
+                                episoden_content['season'],
+                                episoden_content['episode'],
+                                episoden_content['episodeTitle']
+                            ),
+                            'image': serien_content['image']
+                        }
+
+    def __staffelContent(self, url, assetID, soup=None):
+        episoden = []
+        if soup is None:
+            if url is None:
+                return None
+            r = requests.get(url)
+            if r.status_code != 200:
+                print("FEHLER : {0}".format(r.status_code))
+                return None
+            soup = BeautifulSoup(r.text, 'html.parser')
+        for _ep in soup.find_all(
+            "a",
+            class_="teaserset__teaser",
+            attrs={'data-asset-id': assetID}
+        ):
+            url = "http://www.watchbox.de{0}".format(_ep['href'])
+            _info = _ep.find("div", class_='teaser__season-info')
+            if _info:
+                m = re.search(
+                    "Staffel (\d*), Episode (\d*)",
+                    _info.text.replace("\n", "").strip()
+                )
+                if m:
+                    season = int(m.group(1))
+                    episode = int(m.group(2))
+            _header = _ep.find("div", class_="teaser__header")
+            if _header:
+                episodeTitle = _header.text.replace(
+                    "\n", ""
+                ).strip()
+            episoden.append({
+                'season': season,
+                'episode': episode,
+                'episodeTitle': episodeTitle,
+                'url': url
+            })
+        return episoden
+
+    def __serienContent(self, url):
+        r = requests.get(url)
+        if r.status_code != 200:
+            return None
+        soup = BeautifulSoup(r.text, 'html.parser')
+        dataAssetID = soup.find(
+            "div",
+            class_="big-teaser-video"
+        )['data-asset-id']
+        details = {'data-asset-id': dataAssetID, 'seasons': []}
+        _image = soup.find("div", attrs={'class': "video-details__image"})
+        if _image:
+            _img = _image.find('img')
+            if _img['src'].startswith("/"):
+                _img['src'] = "http:{0}".format(_img['src'])
+            details['image'] = _img["src"]
+            details['title'] = _img['alt']
+
+        _seasons = soup.find('ul', class_="season-panel")
+        if _seasons:
+            details['seasons'] = [
+                {
+                    'nr': int(season['data-season-number']),
+                    'title': season.text.replace("\n", "").strip(),
+                    'url': "http://watchbox.de{0}".format(
+                        season.find('a')['href']
+                    )
+                }
+                for season in _seasons.find_all(
+                    "li",
+                    class_="season-panel__item"
+                )
+            ]
+        return details
+
+    def __episodeContent(self, url, soup=None):
+        if soup is None:
+            if url is None:
+                return None
+            r = requests.get(url)
+            if r.status_code != 200:
+                return None
+            soup = BeautifulSoup(r.text, 'html.parser')
+        m = re.search("-(\d*)\.html", url)
+        if m:
+            id_episode = int(m.group(1))
+            _ep = soup.find(
+                "a",
+                class_="teaserset__teaser",
+                attrs={'data-movie-id': str(id_episode)}
+            )
+            if _ep:
+                _info = _ep.find("div", class_='teaser__season-info')
+                if _info:
+                    m = re.search(
+                        "Staffel (\d*), Episode (\d*)",
+                        _info.text.replace("\n", "").strip()
+                    )
+                    if m:
+                        season = int(m.group(1))
+                        episode = int(m.group(2))
+                _header = _ep.find("div", class_="teaser__header")
+                if _header:
+                    episodeTitle = _header.text.replace(
+                        "\n", ""
+                    ).strip()
+        for _js in soup.find_all('script', attrs={'type': 'text/javascript'}):
+            if _js.get("src", None):
+                continue
+            m = re.search("var playerConf = ({.*);", _js.text)
+            if not m:
+                continue
+            playerConf = json.loads(m.group(1))
+            hls = playerConf['source']['hls']
+            dash = playerConf['source']['dash']
+            break
+
+        return {
+            'season': season,
+            'episode': episode,
+            'episodeTitle': episodeTitle,
+            'hls': hls,
+            'dash': dash
+        }
+
 
 
 if __name__ == "__main__":
